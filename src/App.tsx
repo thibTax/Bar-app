e5c692687ac71740996036d35bfa1d7b89e1ffc1
thibTax <thibaultaxil@gmail.com>
2022-11-15 23:26:58 +0100

    configuration of app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+  const name: String = "React";
+
+  return <h1>Hello, {name} !</h1>;
+};
+
+export default App;
